a8ed008d5957b41ec6618725dce45224b44be5cc
ababcock013 <ababcock013@outlook.com>
2022-10-23 17:12:33 -0500

    Added menu and items to RestaurantDbContext

--- DatabaseTest/DbContext/RestaurantDbContext.cs
+++ DatabaseTest/DbContext/RestaurantDbContext.cs
@@ -5,7 +5,16 @@ namespace DatabaseTest.DbContext;
 
 public class RestaurantDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
-    private DbSet<Restaurant> Restaurants { get; set; }
+    private const string connectionString =
+        "User ID=dev;Password=password;Host=localhost;Port=5555;Database=development;";
     
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Menu> Menus { get; set; }
+    public DbSet<Item> Items { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(connectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
 }
